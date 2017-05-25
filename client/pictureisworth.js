10699b67e75ac61306ddb73efec951e7c5e5e3c8
Elliot Hatch <elliot.hatch@gmail.com>
2017-05-24 21:50:45 -0600

    Add spinner animation

--- client/pictureisworth.js
+++ client/pictureisworth.js
@@ -21,10 +21,10 @@
 		var randomChangeInterval = setInterval(function() {
 			$('.spincounter').each(function() {
 				if($(this).attr('data-random')) {
-					//setSpincounter(spincounter, getRandomInt(50, 2500));
+					setSpincounter.call(this, getRandomInt(0, 10000));
 				}
 			});
-		}, 2000);
+		}, 3000);
 
 	function initializeSpincounter() {
 		var element = $(this);
@@ -37,7 +37,7 @@
 					.append(
 				$('<span></span>')
 					.addClass('spinner')
-					.html(['0','1','2','3','4','5','6','7','8','9'].join('</br>'))
+					.html(['0','1','2','3','4','5','6','7','8','9','0','1','2','3','4','5','6','7','8','9'].join('</br>')) //two copies so we can always "scroll up" to the next number
 					.css('top', '0px')
 				));
 		}
@@ -50,17 +50,26 @@
 		setSpincounter.call(element[0], value);
 	}
 
-	function setSpincounter(count) {
+	function setSpincounter(value) {
 		var element = $(this);
 		var digits = element.attr('data-digits') || 2;
+
 		for(var i = 0; i < digits; i++) {
-			var spinner = element.children().eq(i).children().first();
-			//TODO: remove this and do actual animation
-			(function(s) {
-				setInterval(function() {
-					s.css('top', (parseFloat(s.css('top'), 10) - 1) + 'px');
-				}, 1/60);
-			})(spinner);
+			(function(i) {
+				var placeholder = element.children().eq(i);
+				var spinner = placeholder.children().first();
+				//TODO: remove this and do actual animation
+				var digitHeight = spinner.height() / 20;
+				var digitValue = Math.floor(value / Math.pow(10, digits-i-1)) % 10;
+				spinner.css('top', (-digitHeight*(digitValue+10)) + 'px');
+				//when the transition is done, set us back to the top version of the digit so we can scroll down later
+				setTimeout(function() {
+					spinner.addClass('notransition');
+					spinner.css('top', (parseFloat(spinner.css('top')) + digitHeight * 10) + 'px');
+					spinner[0].offsetHeight; //trigger reflow to apply css changes
+					spinner.removeClass('notransition');
+				}, 1500); //this 1500 should match the css for the .spinner transition top duration
+			})(i);
 		}
 	}
 
